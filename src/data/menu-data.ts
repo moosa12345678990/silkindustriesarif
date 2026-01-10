6763deed6a391c1edbe4781001497347f8f4ff1f
moosa12345678990 <moosa.webdeveloper@gmail.com>
2026-01-10 00:18:20 -0800

    mobile menus updated

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -32,29 +32,29 @@ export default menu_data;
 
 // mobile menus 
 export const mobile_menu_data: IMenuDT[] = [
-  {
+   {
     id: 1,
     title: 'Home',
     link: '/',
   },
   {
     id: 2,
-    title: 'Pages',
-    link: '#',
+    title: 'Products',
+    link: '/blog-classic',
   },
   {
     id: 3,
-    title: 'Portfolio',
-    link: '/portfolio-standard',
-  },
-  {
-    id: 4,
-    title: 'Blog',
+    title: 'manufacturing',
     link: '/blog-modern',
   },
   {
+    id: 4,
+    title: 'about',
+    link: '/about-us',
+  },
+  {
     id: 5,
-    title: 'Contact',
+    title: 'contact',
     link: '/contact',
-  }
+  },
 ];
